6ca7e4e1e798e5fcff78d10210be4a6afa9cfbec
finlay <liufy.thu@gmail.com>
2014-09-01 00:18:13 +0800

    update helloworld

--- helloworld.c
+++ helloworld.c
@@ -8,10 +8,64 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
+#include <asm/thread_info.h>
+
+static void print_taskinfo(struct task_struct *task)
+{
+	printk(KERN_ALERT "\ncurrent task PID is %d\n", task->pid);
+	printk(KERN_ALERT "current task state is %ld\n", task->state);
+}
+
+static int list_parent_task(struct task_struct *task)
+{
+	
+	print_taskinfo(task);
+	if (task->pid > 1)
+		list_parent_task(task->parent);			
+	return 0;
+}
+
+static int list_child_task(struct task_struct *task, struct list_head *list)
+{
+	struct task_struct *task1;
+	struct pid *p1;
+	p1 = find_get_pid(1);
+	task1 = get_pid_task(p1, PIDTYPE_PGID);
+	print_taskinfo(task1);
+
+	list_for_each(list, &task1->children)
+	{
+		task = list_entry(list, struct task_struct, sibling);
+		print_taskinfo(task);
+	}
+	return 0;
+}
 
 static int hello_init(void)
 {
+	struct task_struct *test_task;
+	struct list_head *list;
 	printk(KERN_ALERT "I bear a charmed life!\n");
+	printk(KERN_ALERT "sizeof task_struct is %ld\n", sizeof(struct task_struct));
+	printk(KERN_ALERT "sizeof thread_info is %ld\n", sizeof(struct thread_info));
+	
+	test_task = current_thread_info()->task;
+	printk(KERN_ALERT "current task address is %p\n", test_task);
+	print_taskinfo(test_task);
+	//printk(KERN_ALERT "TASK_RUNNING is %p\n", TASK_RUNNING);
+	set_task_state(test_task, TASK_INTERRUPTIBLE);
+	printk(KERN_ALERT "after set:\n");
+	print_taskinfo(test_task);
+	
+	set_task_state(test_task, TASK_RUNNING);
+	printk(KERN_ALERT "after set:\n");
+	print_taskinfo(test_task);
+
+	printk(KERN_ALERT "parent pid:\n");
+	list_parent_task(current);
+	printk(KERN_ALERT "children pid:\n");
+	list_child_task(test_task, list);
 	return 0;
 }
 
