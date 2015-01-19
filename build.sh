5b3e3af5481d14526b725550dfbd672bb80a1aaa
= <=>
2015-01-19 23:38:19 +0800

    feature:changed build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # Created Time: Mon 01 Sep 2014 12:31:30 AM CST
 #########################################################################
 #!/bin/bash
-make 
+make -f makefile
 dmesg -c
 echo ################################
 echo local prints!!!!
